62feeb19ac6baf4856c58e9d792fd03fde585259
pepo <pepo@ukr.net>
2019-06-06 21:01:50 +0300

    fin5

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 
 let div=document.querySelector('.div_1');
 let point=document.querySelector('.point');
+let point2=document.querySelector('.point2');
+
 div.addEventListener('mousemove', function(event) {
-    point.style.left=event.pageX -250+'px';      
-    point.style.top=event.pageY -15+'px'; 
-    point.style.marginLeft="5px";
+    point.style.left=event.pageX-15 +'px';      
+    point.style.top=event.pageY-15 +'px'; 
     
+    point2.style.right=event.pageX -15+'px';      
+    point2.style.bottom=event.pageY-15+'px';
     
 });
-let div3=document.querySelector('.div_3');
-let point2=document.querySelector('.point2');
- div3.addEventListener('mousemove', function(k) {
-    point2.style.right=k.pageX -240+'px';      
-    point2.style.bottom=k.pageY -420+'px'; 
-     
-}); 
+
 
